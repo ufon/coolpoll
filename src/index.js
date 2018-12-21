36f71ad06f1f4694338b8e3e01aee75a4393e98b
Anton Postoyalko <ufon.exe@yandex.ru>
2018-12-21 05:35:49 +0300

    make it better

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
-import FontFaceObserver from "fontfaceobserver";
 import "antd/dist/antd.css";
-import "./fonts/BasisGrotesquePro.css";
 import GlobalStyle from "./global-styles";
-
-const fontObserver = new FontFaceObserver("Basis Grotesque Pro", {});
-
-fontObserver.load().then(() => {
-  document.body.classList.add("fontLoaded");
-});
 
 ReactDOM.render(
   <React.Fragment>
